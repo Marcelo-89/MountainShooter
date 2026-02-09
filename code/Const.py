b1e68a2934313ba89c49a040b63bdedc86a4e77a
MarceloMA <marcelo_ma@hotmail.com.br>
2026-02-09 11:53:28 -0300

    (erro no play) hit box Collision HUD Score

--- code/Const.py
+++ code/Const.py
@@ -4,6 +4,8 @@ import pygame
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 128)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -42,7 +44,39 @@ ENTITY_HEALTH = {
     'Enemy2': 60,
     'Enemy2Shoot': 1,
 }
-
+ENTITY_DAMAGE = {
+'Level1Bg0' : 0,
+    'Level1Bg1' :0,
+    'Level1Bg2' : 0,
+    'Level1Bg3' : 0,
+    'Level1Bg4' : 0,
+    'Level1Bg5' : 0,
+    'Level1Bg6' : 0,
+    'Player1': 1,
+    'Player1Shoot': 25,
+    'Player2': 1,
+    'Player2Shoot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
+ENTITY_SCORE = {
+'Level1Bg1' :0,
+    'Level1Bg2' : 0,
+    'Level1Bg3' : 0,
+    'Level1Bg4' : 0,
+    'Level1Bg5' : 0,
+    'Level1Bg6' : 0,
+    'Player1': 0,
+    'Player1Shoot': 0,
+    'Player2': 0,
+    'Player2Shoot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
     'Player2': 15,
